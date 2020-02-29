8c461470f2bb18d9470abb85a2fa66cecebe6040
vincent1703 <vmoreau1703@gmail.com>
2020-02-29 11:00:02 -0500

    creation et organisation des differents modeles de donnees et de traitement

---
+++ donnees.py
@@ -0,0 +1,29 @@
+image_contour = {
+    "path" : "",
+    "nom": "",
+    "hauteur_originale": "1",
+    "largeur_originale": "1",
+    "difference": "1",
+    "ratio_resize": "1",
+    "hauteur": "1",
+    "largeur": "1",
+}
+
+image_countour_booleen = [[False for i in range(image_contour["largeur"])] for j in range(image_contour["hauteur"])]
+
+
+image_contraste = {
+    "path" : "",
+    "nom": "",
+    "hauteur_originale": "1",
+    "largeur_originale": "1",
+    "threshold": "1",
+    "ratio_resize": "1",
+    "hauteur": "1",
+    "largeur": "1",
+}
+
+image_countraste_booleen = [[False for i in range(image_contraste["largeur"])] for j in range(image_contraste["hauteur"])]
+
+
+
