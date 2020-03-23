5741c500202dd6ac6c767cd699da1324b5cd8a62
vincent1703 <vincent1703@gmail.com>
2020-03-23 15:06:03 -0400

    creation de la premiere partie de linterface

--- gui.py
+++ gui.py
@@ -1,18 +1,42 @@
-from guizero import App, Text, TextBox, PushButton, info
+from guizero import App, Text, TextBox, PushButton, info, Picture
 import donnees
 
 def chercher_fichier():
     donnees.image_path = app.select_file(filetypes=[["Images png", "*.png"], ["Images jpeg", "*.jpeg"],["Images jpg", "*.jpg"], ["Images gif", "*.gif"]])
+    chemin_image.value = donnees.image_path
 
-app = App(layout="grid")
+def confirmer_chemin():
+    donnees.image_path = chemin_image.value
+    global apercu
+    apercu = Picture(app, image=donnees.image_path, grid=[3,0])
+    apercu.resize(150,150)
+
+def confirmer_hauteur():
+    donnees.hauteur_surface = hauteur_boite.value
+    hauteur_boite.value = donnees.hauteur_surface
+
+def confirmer_largeur():
+    donnees.largeur_surface = largeur_boite.value
+    largeur_boite.value = donnees.largeur_surface
+
+app = App(layout="grid", width = 1280, height = 720)
 
 app.info("Bienvenue", "Pour commencer :\n 1) Sélectionnez l'image source\n 2) Sélectionnez les dimensions de la surface d'impression")
 
-#indication_image = Text(app, "Image sélectionnée:", grid=[0,0])
-chemin_image = TextBox(app, "123", grid=[0,1])
-
+chemin_image = TextBox(app, "/home/vincent/bdeb_cnc/images/apercu.png", grid=[1,0], width = 80)
 
 bouton_selection = PushButton(app, command = chercher_fichier, text = "Sélectionner l'image", grid=[0,0])
+bouton_confirmer = PushButton(app, command = confirmer_chemin, text = "Ok", grid=[2,0])
+
+surface_tag = Text(app, "Parametres de l'impression", grid=[1,1])
+largeur_tag = Text(app, "Largeur de la surface (mm) ", grid = [0,2], align="left")
+largeur_boite = TextBox(app, "100", width = 10, grid = [1,2], align="left")
+bouton_confirmer_largeur = PushButton(app, text = "Ok", command = confirmer_largeur, grid = [1,2])
+
+hauteur_tag = Text(app, "Hauteur de la surface(mm)", grid = [0,3], align="left")
+hauteur_boite = TextBox(app, "100", width = 10, grid = [1,3], align="left")
+bouton_confirmer_hauteur = PushButton(app, text = "Ok", command = confirmer_hauteur, grid = [1,3])
+confirmer_chemin()
 
 app.display()
 
